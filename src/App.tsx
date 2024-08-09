ceac6e9cb996048b230c3c0e5322316be08ccaca
Inga Kononova <inga.kononova@gmail.com>
2024-08-09 21:29:11 +0300

    weatherApp

---
+++ src/App.tsx
@@ -0,0 +1,48 @@
+import React, {useState} from 'react';
+import './App.css';
+import Weather from "./Weather";
+
+function App() {
+    //https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API key}
+    //8cc2056e4d936293742a9bf4c38cdad2
+
+    const[city,setCity]=useState<string>('')
+    const [error, setError] = useState<string | null>(null)
+    const [weather, setWeather] = useState<{ temp: number, description: string } | null>(null);
+
+          const fetchWeather = () => {
+            //const city: string = 'London'
+            const APIkey: string = '8cc2056e4d936293742a9bf4c38cdad2'
+            fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIkey}&units=metric`)
+            .then(response => response.json())
+            .then(json => {
+                if (json.cod === "404") {
+                    setError('City not found'); // Устанавливаем ошибку, если город не найден
+                    setWeather(null);
+                } else {
+                    setWeather({
+                        temp: json.main.temp,
+                        description: json.weather[0].description
+                    });
+                    setError(null); // Сбрасываем ошибку, если запрос успешен
+                }
+            })
+            .catch(error => {
+                console.error('Ошибка:', error);
+                setError('An error occurred'); // Общая ошибка на случай других проблем
+                setWeather(null);
+            });
+      }
+
+    return (
+        <div className="App">
+            <h1>Weather App</h1>
+            <input type="text" onChange={(e) => setCity(e.currentTarget.value)}/> 
+            <button onClick={fetchWeather}>Get weather</button>
+            {error &&<div style={{ color: 'red' }}>{error}</div>}
+            {weather &&<Weather temp={weather.temp} description={weather.description}/>}
+        </div>
+    );
+}
+
+export default App;
